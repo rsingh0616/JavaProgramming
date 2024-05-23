b9a30590069beae5b38b7028948dcaf5ce6a83b4
Ripunjay Singh <65473308+rsingh0616@users.noreply.github.com>
2024-05-23 11:30:52 -0400

    Update quadraticapp.java

--- quadraticapp.java
+++ quadraticapp.java
@@ -1,45 +1,38 @@
 import javax.swing.JOptionPane;
 
-public class quadraticapp
-{
-	public static void main (String args[])
-	{                   String    sa, sb, sc, message1="";
-       String message2="", root1="", root2="", result="";
-       double  a,  b, c, x1=0,x2=0, disc;
-      
-      sa = JOptionPane.showInputDialog("Enter the coefficient a ");
-      a = Double.parseDouble(sa);
-            
-      sb = JOptionPane.showInputDialog("Enter the coefficient b ");
-      b = Double.parseDouble(sb);
-     
-      sc = JOptionPane.showInputDialog("Enter the coefficient c ");
-      c = Double.parseDouble(sc);
-     
-      disc=Math.sqrt((b*b)-4*a*c);
-      
-      message1= message1 + "The roots are imaginary";
-      message2= message2 + "The roots exist and they are given below";
-      
-      
-      if(disc<0)
-      { JOptionPane.showMessageDialog(null ,message1);
-      
-      }
-      
-      else
-      {  JOptionPane.showMessageDialog(null, message2);
-      x1= ((-b)+ disc)/(2*a);
-      x2= ((-b)- disc)/(2*a);
-      }
-      
-      root1= root1+ "The first root of the equation is" + x1;
-      root2= root2+ "The second root of the equation is" + x2;
-      result= root1+root2;
+public class QuadraticApp {
+    public static void main(String[] args) {
+        // Declare variables to store user input and calculation results
+        String inputA, inputB, inputC, message;
+        double a, b, c, discriminant, root1, root2;
 
-JOptionPane.showMessageDialog(null, result, "quadratic program", JOptionPane.INFORMATION_MESSAGE);
-      }
+        // Prompt the user to enter the coefficient a
+        inputA = JOptionPane.showInputDialog("Enter the coefficient a: ");
+        a = Double.parseDouble(inputA);
+
+        // Prompt the user to enter the coefficient b
+        inputB = JOptionPane.showInputDialog("Enter the coefficient b: ");
+        b = Double.parseDouble(inputB);
+
+        // Prompt the user to enter the coefficient c
+        inputC = JOptionPane.showInputDialog("Enter the coefficient c: ");
+        c = Double.parseDouble(inputC);
+
+        // Calculate the discriminant (b^2 - 4ac)
+        discriminant = (b * b) - 4 * a * c;
+
+        // Check if the roots are real or imaginary
+        if (discriminant < 0) {
+            // Roots are imaginary if the discriminant is less than 0
+            message = "The roots are imaginary.";
+        } else {
+            // Calculate the two real roots using the quadratic formula
+            root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+            root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+            message = "The roots exist and they are:\nRoot 1: " + root1 + "\nRoot 2: " + root2;
+        }
+
+        // Display the result to the user
+        JOptionPane.showMessageDialog(null, message, "Quadratic Equation Solver", JOptionPane.INFORMATION_MESSAGE);
+    }
 }
-      
-         
-
